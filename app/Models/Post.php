6c4b2ecf4b18d67cb976e9b3a05091b520d7dd9f
sdkakcy <sdkakcy@gmail.com>
2022-07-01 23:07:04 +0300

    category post relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
